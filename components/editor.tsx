e54fe218d0835be2a5db4442dd2f89111b7cf95f
Riddle <muyuhe0327@gmail.com>
2024-01-26 21:27:52 -0500

    Added editor menu item

--- components/editor.tsx
+++ components/editor.tsx
@@ -2,16 +2,19 @@
 
 import { useTheme } from "next-themes";
 import {
-  BlockNoteEditor,
-  PartialBlock
+  BlockNoteEditor, defaultProps,
+  PartialBlock, defaultBlockSchema,
+  defaultBlockSpecs,
 } from "@blocknote/core";
 import {
-  BlockNoteView,
-  useBlockNote
+  BlockNoteView, createReactBlockSpec,
+  useBlockNote, ReactSlashMenuItem,
+    getDefaultReactSlashMenuItems
 } from "@blocknote/react";
 import "@blocknote/core/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
+import {HiOutlineGlobeAlt} from "react-icons/hi";
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -35,6 +38,40 @@ const Editor = ({
     return response.url;
   }
 
+  const insertCode = (editor: BlockNoteEditor) => {
+    // Block that the text cursor is currently in.
+    const currentBlock = editor.getTextCursorPosition().block;
+
+    const formattedCode = `
+    function greet() {
+        console.log("Hello World");
+    }
+    `;
+
+    // New block we want to insert.
+    const codeBlock = {
+      type: "paragraph" as const,
+      content: [{ type: "text", text: formattedCode, styles: { bold: true } }],
+    } as const;
+
+    // Inserting the new block after the current one.
+    editor.insertBlocks([codeBlock], currentBlock, "before");
+  };
+
+  const insertCodeItem: ReactSlashMenuItem = {
+    name: "Insert Code",
+    execute: insertCode,
+    aliases: ["code", "cd"],
+    group: "Other",
+    icon: <HiOutlineGlobeAlt size={18} />,
+    hint: "Inserts a code block",
+  }
+
+  const newSlashMenuItems= [
+      ...getDefaultReactSlashMenuItems(),
+      insertCodeItem,
+  ]
+
   const editor: BlockNoteEditor = useBlockNote({
     editable,
     initialContent: 
@@ -44,7 +81,8 @@ const Editor = ({
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
-    uploadFile: handleUpload
+    slashMenuItems: newSlashMenuItems,
+    uploadFile: handleUpload,
   })
 
   return (
