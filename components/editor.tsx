01fe377ba3238d24fcfa8d42ef4a3cdfd15683b6
Riddle <muyuhe0327@gmail.com>
2024-01-28 02:46:45 -0500

    Solved updateBlock issue

--- components/editor.tsx
+++ components/editor.tsx
@@ -64,7 +64,7 @@ const Editor = ({
             }}
             onChange={(codeMirrorEditor, data, value) => {
               editor.updateBlock(block, {
-                content: [{ type: 'text', text: value, styles: {} }],
+                content: value,
               });
             }}
 
